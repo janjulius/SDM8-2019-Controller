1795262d8ddf8e04b40839e1291d00643929d1d2
Jan Julius <jdelang@hotmail.com>
2019-10-03 14:16:19 +0200

    add: initial project files

---
+++ src/com/sdm8/Main.java
@@ -0,0 +1,20 @@
+package com.sdm8;
+
+/**
+ * Main
+ * @author SDM8
+ * @date Oct 3, 2019
+ */
+public class Main {
+
+	/**
+	 * The main entry point of the program
+	 * @param args Arguments to start the program
+	 * @param args 0 ip default: 91.121.165.36
+	 * @param args 1 port default: 1883
+	 * @param args 2 websocket port default: 8183
+	 */
+    public static void main(String[] args) {
+    	
+    }
+}
---
+++ src/com/sdm8/constants/Constants.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.sdm8.constants;
+
+
+/**
+ * Constants.java
+ * Handles the constant values
+ * @author SDM8
+ * @date Oct 3, 2019
+ */
+public class Constants {
+
+	/**
+	 * The default address to connect to
+	 */
+	public static final String ADDRESS = "91.121.165.36";
+	
+	/**
+	 * The default port
+	 */
+	public static final String PORT = "1883";
+	
+	/**
+	 * The default websocket port
+	 */
+	public static final String WEBSOCKET_PORT = "8183";
+}
